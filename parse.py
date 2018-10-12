4f1c36bc5d0a2abf30840cdb065f361c871d2730
safurrier <safurrier@gmail.com>
2018-10-12 08:41:38 -0700

    Cloned repo and adapted parse.py for my data

--- parse.py
+++ parse.py
@@ -16,12 +16,15 @@ for d in ['BASE','RP','PCA','ICA','RF']:
     if not os.path.exists(n):
         os.makedirs(n)
 
-OUT = './BASE/'
-madX1 = pd.read_csv('./madelon_train.data',header=None,sep=' ')
-madX2 = pd.read_csv('./madelon_valid.data',header=None,sep=' ')
+OUT = 'BASE/'
+
+
+#### Parse Madelon Data
+madX1 = pd.read_csv('data/raw/madelon_train.data',header=None,sep=' ')
+madX2 = pd.read_csv('data/raw/madelon_valid.data',header=None,sep=' ')
 madX = pd.concat([madX1,madX2],0).astype(float)
-madY1 = pd.read_csv('./madelon_train.labels',header=None,sep=' ')
-madY2 = pd.read_csv('./madelon_valid.labels',header=None,sep=' ')
+madY1 = pd.read_csv('data/raw/madelon_train.labels',header=None,sep=' ')
+madY2 = pd.read_csv('data/raw/madelon_valid.labels',header=None,sep=' ')
 madY = pd.concat([madY1,madY2],0)
 madY.columns = ['Class']
 
@@ -44,14 +47,36 @@ mad2 = mad2.dropna(axis=1,how='all')
 mad2.to_hdf(OUT+'datasets.hdf','madelon_test',complib='blosc',complevel=9)
 
 
+### Parse Cars Data
+cars_df = pd.read_csv('data/raw/car.data.txt', header=None, 
+                      names=[
+                          "buying ", 
+                          "maint", 
+                          "doors", 
+                          "persons", 
+                          "lug_boot", 
+                          "safety",
+                          'Class'
+                      ])
+cars_df.head()
 
-digits = load_digits(return_X_y=True)
-digitsX,digitsY = digits
+# Changing to binary classification problem
+# Acceptable, Good and Very Good all become the positive class 1
+# Unacceptable is the negative class 0
+cars_df['Class'] = cars_df['Class'].replace({'unacc':0,'acc':1,'vgood':2,'good':2})
+cars_df['doors'] = cars_df['doors'].replace({'5more':5}).apply(pd.to_numeric)
 
-digits = np.hstack((digitsX, np.atleast_2d(digitsY).T))
-digits = pd.DataFrame(digits)
-cols = list(range(digits.shape[1]))
-cols[-1] = 'Class'
-digits.columns = cols
-digits.to_hdf(OUT+'datasets.hdf','digits',complib='blosc',complevel=9)
+one_hot_columns = pd.get_dummies(cars_df.select_dtypes(include='object')).rename(columns=lambda x: x.replace('-','_'))
+cars_df = pd.concat([one_hot_columns, cars_df[['doors','Class']]], axis=1)
+cars_df.to_hdf(OUT+'datasets.hdf','digits',complib='blosc',complevel=9)
+
+# digits = load_digits(return_X_y=True)
+# digitsX,digitsY = digits
+
+# digits = np.hstack((digitsX, np.atleast_2d(digitsY).T))
+# digits = pd.DataFrame(digits)
+# cols = list(range(digits.shape[1]))
+# cols[-1] = 'Class'
+# digits.columns = cols
+# digits.to_hdf(OUT+'datasets.hdf','digits',complib='blosc',complevel=9)
 
